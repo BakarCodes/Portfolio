bfae5d304b2524125198da1bef672e93c3a57dff
Abubakar Gudal <86916278+BakarCodes@users.noreply.github.com>
2023-07-10 15:26:01 +0100

    Update

--- portfolio/src/Components/Pages/Home.js
+++ portfolio/src/Components/Pages/Home.js
@@ -1,84 +1,28 @@
 import React from 'react'
 import "./Home.css"
-import {
-  MDBCard,
-  MDBCardImage,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardFooter,
-  MDBRow,
-  MDBCol
-} from 'mdb-react-ui-kit';
-
+import novo from "../Images/Novo.jpg"
 
 function Home() {
     return (
     
     <div className='Home'>
-      
         <section class="one">
-
+          <h1 className='title'>
+            BAKAR
+          </h1>
+          <h2 className='subheading'>
+            SOFTWARE DEVELOPER 
+          </h2>
         </section>
         <section class="two">
-          <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-            <MDBCol>
-              <MDBCard className='project-card'>
-                <MDBCardImage
-                  src='https://mdbootstrap.com/img/new/standard/city/044.webp'
-                  alt='...'
-                  position='top'
-                />
-                <MDBCardBody>
-                  <MDBCardTitle>Card title</MDBCardTitle>
-                  <MDBCardText>
-                    This is a longer card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer.
-                  </MDBCardText>
-                </MDBCardBody>
-                <MDBCardFooter>
-                  <small className='text-muted'>Last updated 3 mins ago</small>
-                </MDBCardFooter>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard className='project-card'>
-                <MDBCardImage
-                  src='https://mdbootstrap.com/img/new/standard/city/043.webp'
-                  alt='...'
-                  position='top'
-                />
-                <MDBCardBody>
-                  <MDBCardTitle>Card title</MDBCardTitle>
-                  <MDBCardText>
-                    This card has supporting text below as a natural lead-in to additional content.
-                  </MDBCardText>
-                </MDBCardBody>
-                <MDBCardFooter>
-                  <small className='text-muted'>Last updated 3 mins ago</small>
-                </MDBCardFooter>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard className='project-card'>
-                <MDBCardImage
-                  src='https://mdbootstrap.com/img/new/standard/city/042.webp'
-                  alt='...'
-                  position='top'
-                />
-                <MDBCardBody>
-                  <MDBCardTitle>Card title</MDBCardTitle>
-                  <MDBCardText>
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    card has even longer content than the first to show that equal height action.
-                  </MDBCardText>
-                </MDBCardBody>
-                <MDBCardFooter>
-                  <small className='text-muted'>Last updated 3 mins ago</small>
-                </MDBCardFooter>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
+          <div className='image-grid'>
+            <div className='projectTitle'>
+              Novo
+            </div>
+              <div className='image'>
+                <img src={novo} className='novo' alt='novo_image'/>
+              </div>
+          </div>
         </section>
     </div>
     
